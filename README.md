37e1df0e0388d329f2137b453f7c3172f7e3668f
Toby <toby.bettridge@stubside.com>
2014-01-31 12:40:32 +0000

    First commit of code

--- README.md
+++ README.md
@@ -2,3 +2,70 @@ html-deconstruct
 ================
 
 HTML Deconstructor
+
+A simple script to create a deconstructed version of some HTML to make working out the structure a little easier.
+
+## Cutting down HTML
+###Style using only semantic HTML selectors.
+
+So when you see:
+
+`<div class="column-12 content">`
+
+And the current css is:
+
+`div.column-12 { width: 960px; }`
+
+Re-work to:
+
+`div.content { width: 960px; }`
+
+### Remove non-semantic HTML
+When you see:
+
+`<div class="column-12 content">`
+
+Change the HTML to:
+
+`<div class="content">`
+
+Or even:
+
+`<article>`
+
+### Go through each element (within reason) and strip out unused classes
+Use the inspector tools in Chrome or whatever browser tools you usually use and
+click on an element. If it has no rules targetting it specifically on that 
+class then remove the class.
+
+There should be a note of caution here because if there is javascript using
+those classes then you will break it. If you have access (and the ability) then
+you can alter the javascript to target more semantically.
+
+If you have javascript targetting a class of 'clearfix' or 'column-12' then you
+have my sympathies.
+
+### Document autonomous chunks of HTML in a clear and semantic way
+So:
+
+    <header>
+    <a id="main-content"></a>
+      <h1>
+        <span class="artist">{{ artwork.all_artists|capfirst }}</span>
+        <span class="title-row">
+          <span class="title">{{ artwork.title }}</span>
+          <span class="datetext">{{ artwork.dateText }}</span>
+        </span>
+      </h1>
+    </header>
+
+Could become:
+
+    header
+      a#main-content
+      h1
+        span.artist
+        span.title-row
+          span.title
+          span.datetext
+
---
+++ htmldecon.py
@@ -0,0 +1,85 @@
+import urllib2
+import sys
+import os
+
+from HTMLParser import HTMLParser
+
+class StyleTileParser(HTMLParser):
+    """
+    Scans a chunk of HTML and trims it down to tags classes and IDs
+    """
+    depth = 0
+    indent_width = 2
+    output = []
+    tags_to_ignore = ['meta','style','strong','em','b','i']
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+
+    def tag_parser(self, tag, attributes):
+        if tag in self.tags_to_ignore:
+            return
+
+        attrs = dict(attributes)
+        output = " " * (self.indent_width * self.depth) + tag
+
+        if attrs.has_key('id'):
+            output += "#" + attrs['id']
+
+        if attrs.has_key('class'):
+            classes = attrs['class'].split()
+            output += "." + ".".join(classes)
+    
+        if tag == 'div' and len(attrs) == 0:
+            output += "    <<-- Div with no attributes"
+
+        return output
+ 
+    def handle_starttag(self, tag, attrs):
+
+        output = self.tag_parser(tag,attrs) 
+        if output:
+            self.output.append(output)
+            self.depth += 1
+
+    def handle_startendtag(self, tag, attrs):
+
+        output = self.tag_parser(tag,attrs) 
+        if output:
+            self.output.append(output)
+
+    def handle_endtag(self, tag):
+        if tag in self.tags_to_ignore:
+            return
+        if self.depth != 0:
+            self.depth -= 1
+        else:
+            raise Exception("Depth level error - too many close tags")
+
+    def handle_data(self, data):
+        pass
+
+    def handle_entityref(self, name):
+        pass
+
+    def handle_charref(self, name):
+        pass
+
+f = sys.stdin
+for arg in sys.argv[1:]:
+    print "Using html from %s" % arg
+    if arg.startswith("http"):
+        f = urllib2.urlopen(arg)
+    else:
+        f = open(arg, 'r')
+    break
+
+html = f.read()
+f.close()
+parser = StyleTileParser()
+parser.feed(html)
+if len(parser.output):
+    print "\n".join(parser.output)
+else:
+    print "No output"
+
